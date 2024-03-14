f9c7a51e0a9a718f5c960fe661ca83287b4c3e33
Rodrigo Rio <rodrigo.rio@outlook.com.ar>
2024-03-14 12:27:31 -0300

    Clase 1

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// const anio_nacimiento = 1900;
+// const anioNacimiento = 1996;
+//
+// let numeroDeTelefono = "";
+
+// ASIGNACIÓN
+
+// const anioNacimiento = 1996;
+//
+// let edad = 0;
+// edad = 21;
+//
+// let direccion = "";
+
+// OPERACIONES CON VARIABLES
+
+// const numero1 = 10;
+// const numero2 = 30;
+// const numero3 = 50;
+// //          5                50   /     10
+// const resultadoDivision = numero3 / numero1;
+//
+// //           1500                  30   *    50
+// const resultadoMultiplicacion = numero2 * numero3;
+//
+// const resultadoSuma = numero1 + numero2 + numero3 - numero1 - numero2;
+//
+// const string1 = "HOLA";
+// const string2 = "CÓMO ESTÁS";
+// const string3 = ", ";
+//
+// // "HOLACOMO ESTAS" = "HOLA" + "CÓMO ESTÁS"
+// const saludo = string1 + string2;
+//
+// //    HOLA, CÓMO ESTÁS
+// const saludoConEspacios = string1 + string3 + string2;
+//
+// // CONSOLE.LOG
+// console.log("El resultado de la multiplicación es: " + resultadoMultiplicacion);
+
+// PROMPT
+
+// const nombre = prompt("Ingrese el nombre");
+// const edad = prompt("Ingrese la edad");
+//
+// const mensajeDeBienvenida = "Hola " + nombre + ", tu edad es: " + edad;
+// console.log(mensajeDeBienvenida);
+// alert(mensajeDeBienvenida);
+
+// TIPO DE DATO EN PROMPT
+// let edad = parseInt(prompt("Ingrese la edad"));
+// edad = edad + 1;
+//
+// console.log(edad);
+
+let precioDeProducto = parseFloat(prompt("Ingrese el precio del producto"));
+
+console.log("El precio del producto es: $" + precioDeProducto);
+
+// CONCATENAR STRINGS CON NUMEROS
+// const string1 = "hola";
+// const numero2 = 50;
+// const numero3 = 40;
+// const numero1 = 30;
+//
+// const concatenar = numero2 + numero3 + numero1 + string1;
+// console.log(concatenar);
