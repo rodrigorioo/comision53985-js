9cd8a35d4eb147ab703c5040517ab083484af9b5
Rodrigo Rio <rodrigo.rio@outlook.com.ar>
2024-03-21 12:04:28 -0300

    Ciclos

--- js/main.js
+++ js/main.js
@@ -1,145 +1,300 @@
-// IF
+// console.log("5");
+// console.log("5");
+// console.log("5");
+// console.log("5");
+// console.log("5");
 
-// let nombre = prompt("Ingrese el nombre");
-//
-// if (nombre == "Rodrigo") {
-//     console.log("Hola Rodrigo");
-// }
-//
-// if (nombre == "Pepe") {
-//     console.log("Hola pepe, cómo estás?");
+// for (let i = 0; i < 10; i++) {
+//     console.log("Hola como estás");
 // }
 
-// IF - ELSE
 
-// const nombre = prompt("Ingres el nombre");
+/**
+ * EJEMPLOS DE FOR
+ */
+
+// Sin for
+// const numero1 = parseInt(prompt("Ingrese un número"));
+// const numero2 = parseInt(prompt("Ingrese un número"));
+// const numero3 = parseInt(prompt("Ingrese un número"));
+// const numero4 = parseInt(prompt("Ingrese un número"));
+// const numero5 = parseInt(prompt("Ingrese un número"));
 //
-// if (nombre == "Rodrigo") {
-//     console.log("Hola Rodrigo, cómo estás?");
-// } else {
-//     console.log("Hola quien quiera que seas");
+// const resultado = numero1 + numero2 + numero3 + numero4 + numero5;
+
+// Con for
+// let resultado = 0;
+// for (let i = 0; i < 5; i++) {
+//     resultado = resultado + parseInt(prompt("Ingrese un número"));
+// }
+//
+// console.log(resultado);
+
+// Ejemplo con variable
+// const cantidadDeNombres = parseInt(prompt("Ingrese la cantidad de nombres a ingresar"));
+//
+// for (let i = 0; i < cantidadDeNombres; i++) {
+//     const nombre = prompt("Ingrese un nombre");
+//
+//     console.log(nombre);
 // }
 
-// IF, ELSE IF Y ELSE
+/**
+ * SENTENCIA BREAK
+ */
 
-// const nombre = prompt("Ingres el nombre");
+// const cantidadDeNombres = parseInt(prompt("Ingrese la cantidad de nombres a ingresar"));
 //
-// if (nombre == "Rodrigo") {
-//     console.log("Hola Rodrigo, cómo estás?");
-// } else if (nombre == "Pedro") {
-//     console.log("Hola Pedro, todo bien?");
-// } else if (nombre == "Juan") {
-//     console.log("Que onda Juan");
-// } else {
-//     console.log("Hola quien quiera que seas");
+// for (let i = 0; i < cantidadDeNombres; i++) {
+//     const nombre = prompt("Ingrese un nombre");
+//
+//     if(nombre == "Carlos") {
+//         console.log("Salimos del for");
+//         break;
+//     }
+//
+//     console.log(nombre);
+// }
+//
+// console.log("Fin del programa");
+
+/**
+ * SENTENCIA CONTINUE
+ */
+
+// const cantidadDeNombres = parseInt(prompt("Ingrese la cantidad de nombres a ingresar"));
+//
+// for (let i = 0; i < cantidadDeNombres; i++) {
+//     const nombre = prompt("Ingrese un nombre");
+//
+//     if(nombre == "Juan") {
+//         continue;
+//     }
+//
+//     console.log(nombre);
+// }
+//
+// console.log("Fin del programa");
+
+// Ejemplo de continue #2
+// for (let i = 0; i < 10; i++) {
+//
+//     if(i % 2 != 0) {
+//         continue;
+//     }
+//
+//     console.log(i);
 // }
 
-// VARIABLES BOOLEANAS
+/**
+ * ESTRUCTURA DE WHILE
+ */
 
-// const nombre = prompt("Ingres el nombre");
+// let opcion = parseInt(prompt("Ingrese 1 para mostrar un mensaje\n2 para ingresar un nombre\n3 para mostrar otro mensajito"));
 //
-// const esRodrigo = (nombre == "Carlos");
+// while (opcion != 1 && opcion != 2 && opcion != 3) {
+//     alert("Opción incorrecta.")
+//     opcion = parseInt(prompt("Ingrese 1 para mostrar un mensaje\n2 para ingresar un nombre\n3 para mostrar otro mensajito"));
+// }
 //
-// if (esRodrigo) {
-//     console.log("Hola rodrigo cómo estás");
-// } else {
-//     console.log("No sos Rodrigo");
+// if(opcion == 1) {
+//     console.log("Mensajito");
+// } else if(opcion == 2) {
+//     const nombre = prompt("Ingrese un nombre");
+//     console.log(nombre);
+// } else if(opcion == 3) {
+//     console.log("Otro mensajito");
 // }
 
-// OPERADORES LÓGICOS
+/**
+ * ESTRUCTURA DO WHILE
+ */
+
+// do {
+//     opcion = parseInt(prompt("Ingrese 1 para mostrar un mensaje\n2 para ingresar un nombre\n3 para mostrar otro mensajito"));
+//
+// } while (opcion != 1 && opcion != 2 && opcion != 3);
+//
+// if(opcion == 1) {
+//     console.log("Mensajito");
+// } else if(opcion == 2) {
+//     const nombre = prompt("Ingrese un nombre");
+//     console.log(nombre);
+// } else if(opcion == 3) {
+//     console.log("Otro mensajito");
+// }
+
+/**
+ * SWITCH
+ */
+
+// let opcion = parseInt(prompt("Ingrese 1 para mostrar un mensaje\n2 para ingresar un nombre\n3 para mostrar otro mensajito"));
+//
+// while (opcion != 1 && opcion != 2 && opcion != 3) {
+//     alert("Opción incorrecta.")
+//     opcion = parseInt(prompt("Ingrese 1 para mostrar un mensaje\n2 para ingresar un nombre\n3 para mostrar otro mensajito"));
+// }
+//
+// switch (opcion) {
+//     case 1:
+//         console.log("Mensajito");
+//         break;
+//
+//     case 2:
+//         const nombre = prompt("Ingrese un nombre");
+//         console.log(nombre);
+//         break;
+//
+//     case 3:
+//         console.log("Otro mensajito");
+//         break;
+//
+//     default:
+//         console.log("a");
+//         break;
+// }
+
+// Ejemplo #2
 
 // const nombre = prompt("Ingrese un nombre");
 //
-// if(nombre != "Juan") {
-//     console.log("No sos Juan");
-// }
-
-// const precio = 400;
+// switch (nombre) {
+//     case "Pedro":
+//         console.log("Hola pedro");
+//         break;
 //
-// if (precio > 500) {
-//     console.log("Mayor a 500");
+//     case "Daniel":
+//         console.log("Hola daniel");
+//         break;
+//
+//     default:
+//         console.log("Nombre incorrecto");
+//         break;
+// }
+
+// Calculadora
+
+// let operacion = prompt("Ingrese la operación a realizar.\n+ Para suma\n- para resta\n* para multiplicación\n INGRESE CUALQUIER LETRA PARA SALIR");
+//
+// while( operacion == "+" || operacion == "-" || operacion == "*") {
+//
+//     const numero1 = parseInt(prompt("Ingrese un número"));
+//     const numero2 = parseInt(prompt("Ingrese un número"));
+//
+//     switch(operacion) {
+//
+//         case "+":
+//             console.log("La suma es: " + (numero1 + numero2));
+//             break;
+//
+//         case "-":
+//             console.log("La resta es: " + (numero1 - numero2));
+//             break;
+//
+//         case "*":
+//             console.log("La multiplicación es: " + (numero1 * numero2));
+//             break;
+//
+//         default:
+//             alert("Operación no válida");
+//             break;
+//     }
+//
+//     operacion = prompt("Ingrese la operación a realizar.\n+ Para suma\n- para resta\n* para multiplicación\nINGRESE CUALQUIER LETRA PARA SALIR");
 // }
 //
-// if (precio < 500) {
-//     console.log("Menor a 500");
+// console.log("Usted salió de la calculadora");
+
+// Ejemplo de carrito
+
+// let ingresarProducto = prompt("Ingrese Si para ingresar producto o cualquier letra para no");
+// let total = 0;
+//
+// while (ingresarProducto == "Si") {
+//
+//     const nombreProducto = prompt("Nombre de producto");
+//     const precioProducto = parseInt(prompt("Precio del producto"));
+//
+//     total = total + precioProducto;
+//
+//     console.log(nombreProducto + ": $" + precioProducto);
+//
+//     ingresarProducto = prompt("Ingrese Si para ingresar producto o cualquier letra para no");
 // }
 //
-// if (precio >= 500) {
-//     console.log("Mayor o igual a 500");
+// console.log("El total de los productos son: $" + total);
+
+/**
+ * EJERCICIOS
+ *
+ * 15) Realizar un programa en donde el usuario ingrese un día de la semana y se le
+ * muestre en pantalla:
+ * a) Si es el comienzo de la semana (Lunes - Miercoles)
+ * b) Si es fin de semana (Sabado y Domingo)
+ * c) Si es mitad de semana (Jueves - Viernes)
+ */
+
+// let dia = prompt("Ingrese el día");
+//
+// while (dia != "Lunes" && dia != "Martes" && dia != "Miercoles" && dia != "Jueves" && dia != "Viernes" && dia != "Sabado" && dia != "Domingo") {
+//     dia = prompt("Ingresa un día");
 // }
 //
-// if (precio <= 500) {
-//     console.log("Menor o igual a 500");
+// switch(dia) {
+//     case "Lunes":
+//     case "Martes":
+//     case "Miercoles":
+//         console.log("Comienzo de semana");
+//         break;
+//
+//     case "Jueves":
+//     case "Viernes":
+//         console.log("Mitad de semana");
+//         break;
+//
+//     case "Sabado":
+//     case "Domingo":
+//         console.log("Fin de semana");
+//         break;
 // }
 
-// OPERADORES && Y ||
+/**
+ * EJERCICIOS
+ *
+ * 12) Realizar un programa que le pida al usuario cuántos números quiere ingresar y entre
+ * todos los que ingrese calcular cual es el mayor, el menor y la media.
+ */
 
-// const nombre = prompt("ingrese un nombre");
-// const edad = parseInt(prompt("Ingrese la edad"));
+let mayor = 0;
+let menor = 0;
+let media = 0;
 
-// if ( (nombre == "Rodrigo") && (edad > 18)) {
-//     console.log("Acceso permitido");
-// }
+const cantidadNumeros = parseInt(prompt("Ingrese cantidad de numeros a ingresar"));
 
-// if ( (nombre == "Rodrigo") || (edad > 18) ) {
-//     console.log("Acceso permitido");
-// }
+// Le pedimos el primer número
+if(cantidadNumeros > 0) {
+    const numero = parseInt(prompt("Ingrese un número"));
 
-// ---------
-
-// const nombre = prompt("ingrese un nombre");
-// const edad = parseInt(prompt("Ingrese la edad"));
-
-// ESTO ESTA BIEN
-// if ( (nombre == "Rodrigo" || nombre == "rodrigo") && (edad > 18) ) {
-//     console.log("Acceso permitido");
-// }
-
-// ESTO ESTA MAL!!!!!!
-// if ( nombre == "Rodrigo" || (nombre == "rodrigo" && edad > 18) ) {
-//     console.log("Acceso permitido");
-// }
-
-// OPERADOR ESTRICTAMENTE IGUAL / DISTINTO
-
-// const edad = parseInt(prompt("Ingrese la edad"));
-
-// ESTRICTAMENTE IGUAL
-// if (edad == 20) {
-//     console.log("Usted tiene 20");
-// }
-
-// if (edad === 20) {
-//     console.log("Usted tiene 20");
-// }
-
-// ESTRICTAMENTE DISTINTO
-// if (edad != 20) {
-//     console.log("Edad distinta de 20");
-// }
-
-// if (edad !== 20) {
-//     console.log("Edad distinta de 20");
-// }
-
-// OPERADOR NOT
-
-const edad = parseInt(prompt("Ingrese la edad"));
-
-const esMayorA18 = (edad > 18);
-
-if (esMayorA18) {
-    console.log("Es mayor a 18");
+    mayor = numero;
+    menor = numero;
 }
 
-// ...
+for(let i = 0; i < cantidadNumeros - 1; i++) {
 
-if (esMayorA18) {
-    console.log("Es mayor a 18.");
+    const numero = parseInt(prompt("Ingrese un número"));
+
+    // Calculo la media
+    media = media + numero;
+
+    if(numero > mayor) {
+        mayor = numero;
+    }
+
+    if(numero < menor) {
+        menor = numero;
+    }
 }
 
-// ...
-
-if (!esMayorA18) {
-    console.log("NO ES MAYOR A 18");
-}
+console.log("La media es: " + (media / cantidadNumeros));
+console.log("El mayor es: " + mayor);
+console.log("El menor es: " + menor);
